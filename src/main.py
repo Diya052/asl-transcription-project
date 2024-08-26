74e2e4db3cf97fe56e2f70c6b3f6f9b48a048942
Kaimal, Diya <dkaimal3@gatech.edu>
2024-08-26 16:48:36 -0400

    Initial data preprocessing

--- src/main.py
+++ src/main.py
@@ -1,3 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from PIL import Image
+import tensorflow as tf
+
+
+train_dataset_path = 'data/training'
+test_dataset_path = 'data/test'
+
+train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale = 1./255)
+
+train_generator = train_datagen.flow_from_directory(train_dataset_path, color_mode = 'rgb', class_mode = 'categorical', shuffle = 'True')
+
+test_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale = 1./255)
+
+test_generator = test_datagen.flow_from_directory(test_dataset_path, color_mode = 'rgb', class_mode = 'categorical', shuffle = 'True')
+
+labels = {value: key for key, value in train_generator.class_indices.items()}
+print("Labels for all classes\n")
+for key, value in labels.items():
+    print(f"{key} : {value}")
+
+
+
